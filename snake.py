17824a98d995eb8f86b376d270fe90b9a5e98ff5
luisferro2 <a01378248@itesm.mx>
2021-05-06 18:31:38 -0500

    Importar funciones necesarias de turtle

--- snake.py
+++ snake.py
@@ -1,15 +1,9 @@
-"""Snake, classic arcade game.
-
-Exercises
-
-1. How do you make the snake faster or slower?
-2. How can you make the snake go around the edges?
-3. How would you move the food?
-4. Change the snake to respond to arrow keys.
+"""
 
 """
 
-from turtle import *
+# Related third party libraries.
+from turtle import update, ontimer, setup, hideturtle, tracer, listen, onkey, done, clear
 from random import randrange
 from freegames import square, vector
 
@@ -17,14 +11,17 @@ food = vector(0, 0)
 snake = [vector(10, 0)]
 aim = vector(0, -10)
 
+
 def change(x, y):
     "Change snake direction."
     aim.x = x
     aim.y = y
 
+
 def inside(head):
     "Return True if head inside boundaries."
     return -200 < head.x < 190 and -200 < head.y < 190
+
 
 def move():
     "Move snake forward one segment."
@@ -54,9 +51,12 @@ def move():
     update()
     ontimer(move, 100)
 
+# Initial steps.
 setup(420, 420, 370, 0)
 hideturtle()
 tracer(False)
+
+# Always active.
 listen()
 onkey(lambda: change(10, 0), 'Right')
 onkey(lambda: change(-10, 0), 'Left')
