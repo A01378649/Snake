4fabdeabd0feb4665e723c5d62f519ca4576a545
luisferro2 <a01378248@itesm.mx>
2021-05-06 20:06:04 -0500

    Escribir comentarios de bloque y de línea siguiendo la convención de estilo PEP-8. Oraciones completas. 2 espacios después de expresiones para los comentarios de línea. En inglés. Se incluye espacio después del #.

--- snake.py
+++ snake.py
@@ -9,9 +9,11 @@ from turtle import update, ontimer, setup,     \
 from random import randrange
 from freegames import square, vector
 
+# Global variables.
 food = vector(0, 0)
-snake = [vector(10, 0)]
+snake = [vector(10, 0)]  # List of vectors.
 aim = vector(0, -10)
+# Set of colors.
 five_colors = {"green", "blue", "orange", "turquoise", "yellow"}
 
 
@@ -28,43 +30,50 @@ def inside(head):
 
 def move():
     "Move snake forward one segment."
-    head = snake[-1].copy()
-    head.move(aim)
+    head = snake[-1].copy()  # Copy of the last vector in snake.
+    head.move(aim)  # Modify copied vector with direction of aim.
 
+    # Losing conditions.
     if not inside(head) or head in snake:
         square(head.x, head.y, 9, "red")
         update()
         return
 
-    snake.append(head)
+    snake.append(head)  # Add a position to the list.
 
+    # Snake eating the food.
     if head == food:
         print("Snake:", len(snake))
         food.x = randrange(-15, 15) * 10
         food.y = randrange(-15, 15) * 10
     else:
-        snake.pop(0)
+        snake.pop(0)  # Remove the added position, no food eaten.
 
     clear()
 
+    # Draw the snake.
     global snake_color
     for body in snake:
         square(body.x, body.y, 9, snake_color)
 
+    # Draw the food.
     global food_color
     square(food.x, food.y, 9, food_color)
 
-    update()
-    ontimer(move, 100)
+    update()  # Update the screen when tracer is off.
+
+    ontimer(move, 100)  # Wait 100 ms to call move.
 
 
 # Initial steps.
 setup(420, 420, 370, 0)
-hideturtle()
-tracer(False)
+hideturtle()  # Turtle invisible.
+tracer(False)  # Turns off the turtle animation.
 
-# Always active.
+# Collect key events.
 listen()
+
+# Functions to be run after certain keys are pressed.
 onkey(lambda: change(10, 0), "Right")
 onkey(lambda: change(-10, 0), "Left")
 onkey(lambda: change(0, 10), "Up")
@@ -76,4 +85,4 @@ five_colors.remove(food_color)
 snake_color = next(iter(five_colors))
 
 move()
-done()
+done()  # The event loop.
